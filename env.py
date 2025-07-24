fce5bd69fc1279b490e450f884d5fd904895c7e5
SAreebZaheer <sareebzaheer@gmail.com>
2025-07-24 20:24:39 +0500

    made changes, added functionality of LIDAR

--- env.py
+++ env.py
@@ -3,6 +3,8 @@
 import math
 import pygame
 
+pygame.init()
+
 class buildEnvironment:
     def __init__(self,MapDimensions):
         pygame.init()
@@ -12,7 +14,7 @@ class buildEnvironment:
         self.MapWindowName = 'RRT Path Planning'
         pygame.display.set_caption(self.MapWindowName)
         self.map = pygame.display.set_mode((self.mapw,self.maph))
-        self.map.blit(self.externalMap(0,0))
+        self.map.blit(self.externalMap,(0,0))
 
         #Colours
         self.black = (0, 0, 0)
@@ -21,3 +23,21 @@ class buildEnvironment:
         self.Green = (0, 255, 0)
         self.Red = (255, 0, 0)
         self.white = (255, 255, 255)
+
+    def AD2pos(self,distance,angle,robotPos):
+        x = distance * math.cos(angle) + robotPos[0]
+        y = -distance * math.sin(angle) + robotPos[1]
+        return (int(x), int(y))
+    
+    def dataStorage(self,data):
+        print(len(self.pointCloud))
+        if data!=False:
+            for element in data:
+                point = self.AD2pos(element[0], element[1], element[2])
+                if point not in self.pointCloud:
+                    self.pointCloud.append(point)
+
+    def show_sensorData(self):
+        self.infomap = self.map.copy()
+        for point in self.pointCloud:
+            self.infomap.set_at((int(point[0]),int(point[1])),(255,0,0))
--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import sensors,env
+import pygame
+
+pygame.init()
+
+envi = env.buildEnvironment((600,1200))
+envi.originalMap = envi.map.copy()
+laser = sensors.laserSensor(200,envi.originalMap,uncertainty=(0.5,0.01))
+envi.map.fill((0,0,0))
+envi.infomap = envi.map.copy()
+running = True
+
+while running :
+    sensorOn = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_focused():
+            sensorOn = True
+        elif not pygame.mouse.get_focused():
+            sensorOn = False
+    if sensorOn:
+        pos = pygame.mouse.get_pos()
+        laser.position = pos
+        sensor_data = laser.sense_obstacles()
+        envi.dataStorage(sensor_data)
+        envi.show_sensorData()
+    envi.map.blit(envi.infomap,(0,0))
+    pygame.display.update()
+
+pygame.quit()
+
+""" import env,sensors
 import pygame,math
 
-
-environment = env.buildEnvironment((600,1200))
+envi = env.buildEnvironment((600,1200))
 running = True
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    pygame.display.update()
+    pygame.display.update """
